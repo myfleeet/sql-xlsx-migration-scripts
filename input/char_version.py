33c0694c336b40acc1a817fa0d026d80926e23f5
4YabPPb8 <jorge.nova@astara.com>
2023-04-27 10:13:44 +0200

    chore(goto): queries

---
+++ input/char_version.py
@@ -0,0 +1,24 @@
+import utils
+
+output_file = 'Charact_PA_Version'
+
+query = """
+select 
+    distinct TRIM(vc."version") as "version"
+from
+	vehicle_classifications vc
+order by
+	"version"
+;  
+"""
+
+mock_query_response = dict(
+  version=''
+)
+
+def serialized_data(elm = mock_query_response):
+  return {
+    'Clave de idioma': 'ES',
+    'Acabado': utils.version_code(elm),
+    'Denominacion valor de  caracteristica': elm.get('version'),
+  }
