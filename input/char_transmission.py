b8c0167a3f0ae72f21d9ccdcf75c5f0af23f578f
4YabPPb8 <jorge.nova@astara.com>
2023-04-27 10:13:24 +0200

    chore(goto): queries

---
+++ input/char_transmission.py
@@ -0,0 +1,26 @@
+import utils
+
+output_file = 'Charact_PA_Transmission'
+
+query = """
+select 
+	distinct trim(vc.specs ->> 'shift') AS shift   
+from
+	vehicle_classifications vc
+where
+  vc.specs ->> 'shift' is not null
+order by
+	shift
+;  
+"""
+
+mock_query_response = dict(
+  shift=''
+)
+
+def serialized_data(elm = mock_query_response):
+  return {
+    'Clave de idioma': 'ES',
+    'Transmision': utils.shift_code(elm), 
+    'Denominacion valor de  caracteristica': utils.shift_desc(elm), 
+  }
