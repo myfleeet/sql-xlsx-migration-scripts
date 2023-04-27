d11b84535f136b8dcbbce441cba2dd258a7cd893
4YabPPb8 <jorge.nova@astara.com>
2023-04-27 10:12:51 +0200

    chore(goto): queries

---
+++ input/_goto_supplier.py
@@ -0,0 +1,40 @@
+import utils
+
+output_file = 'Suppliers Layout'
+
+query = """
+select 
+	s.id,
+	s.cif,
+	s.contact_person, -- name if not
+	s.created_at,
+	s.email,
+	s."name",
+	s.phone,
+	s.billing_address
+from suppliers s 
+;
+"""
+
+mock_query_response = dict( 
+  id='', cif='', contact_person='', created_at='', email='', name='', phone='', billing_address='',
+)
+
+def serialized_data(elm = mock_query_response):
+  return {
+    'id': elm.get('id'),
+    'cif': elm.get('cif'),
+
+    # 🔴 Confirmar esta lógica
+    'contact_person': elm['contact_person'] if elm['contact_person'] else elm['name'],
+
+    'created_at': elm.get('created_at'),
+    'email': elm.get('email'),
+    'name': elm.get('name'),
+    'phone': elm.get('phone'),
+    'billing_address': utils.user_address(elm),
+    'billingaddress_details': utils.user_address_details(elm),
+    'billingmunicipality': utils.user_city(elm),
+    'billingzip_code': utils.user_material_code(elm),
+  }
+
