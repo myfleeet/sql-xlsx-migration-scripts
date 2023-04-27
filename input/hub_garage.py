afa411920ef7b835d26928f87cc9ee52ad177479
4YabPPb8 <jorge.nova@astara.com>
2023-04-27 10:14:08 +0200

    chore(accenture): queries

---
+++ input/hub_garage.py
@@ -0,0 +1,19 @@
+output_file = 'Astara Move - Service per Hub&Garages'
+
+query = """
+select 
+  name, 
+  type 
+from hubs h ; 
+"""
+
+mock_query_response = dict(
+  type='', name='', 
+)
+
+def serialized_data(elm = mock_query_response):
+  return {
+    "type": elm.get('type'),
+    "name of supplier": elm.get('name'),
+    "service performed": None,
+  }
