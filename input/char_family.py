bf102ef2f2743a8aa86ed15e14dcb84eb086b4eb
4YabPPb8 <jorge.nova@astara.com>
2023-04-27 10:13:10 +0200

    chore(goto): queries

---
+++ input/char_family.py
@@ -0,0 +1,24 @@
+import utils
+
+output_file = 'Charact_PA_Family'
+
+query = """
+select 
+    distinct TRIM(vc."model") as "model"
+from
+	vehicle_classifications vc
+order by
+	"model"
+;
+"""
+
+mock_query_response = dict(
+  model=''
+)
+
+def serialized_data(elm = mock_query_response):
+  return {
+    'Clave de idioma': 'ES',
+    'Gran Modelo': utils.family_code(elm),
+    'Denominacion valor de  caracteristica': utils.family_desc(elm) ,
+  }
