eeee33bc84da31817a93743c89217a084ffac1d1
4YabPPb8 <jorge.nova@astara.com>
2023-04-27 11:52:01 +0200

    chore(accenture): updated script

--- input/hub_garage.py
+++ input/hub_garage.py
@@ -8,12 +8,13 @@ from hubs h ;
 """
 
 mock_query_response = dict(
-  type='', name='', 
+  type='',
+  name='',
 )
 
 def serialized_data(elm = mock_query_response):
   return {
     "type": elm.get('type'),
     "name of supplier": elm.get('name'),
-    "service performed": None,
+    "service performed": 'inspection',
   }
