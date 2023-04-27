da889104469b8511c879c7543bc242ecb19d3043
4YabPPb8 <jorge.nova@astara.com>
2023-04-27 10:14:13 +0200

    chore(accenture): queries

--- input/material.py
+++ input/material.py
@@ -1,3 +1,5 @@
+import utils
+
 output_file = 'Astara Move - Materials'
 
 query = """
@@ -12,78 +14,34 @@ mock_query_response = dict(
 )
 
 def serialized_data(elm = mock_query_response):
-  rm_chars = lambda str: ''.join([char for char in str if char not in [" ", ".", "-", "_", "(", ")"]])
-
   return {
-    # ok - FLET
     'company_code':'FLET',
-
-    # ok - MAKEMODEL
-    'comercial_model' : rm_chars(elm['model'].upper())[:18],
-
-    # ok - Z
+    'comercial_model' : utils.commercial_model(elm),
     'type':'Z',
- 
-    # ok - VEHI
     'tipo_de_material':'VEHI',
-
-    # ok - FESP
     'plant':'FESP',
-
-    # ok - FLES
     'sales_organization':'FLES',
-
-    # ok - 01
     'distribution_channel':None,
-
-    # ok - MAKE MODEL VERSION
-    'description' : elm['make'] + ' ' + elm['model'],
-
-    # ok - UN
+    'description' : utils.make_model(elm),
     'measure_unit':'UN',
-
     'group_article':'ZMOD_VEH',
-
     'brand':'AM',
-
     'hierarchy':'0KIACVEHICVLIGVH0A',
-
     'position_type':'VMS0',
-
     'fixed_indicator':'X',
-
     'fiscal_classification':1,
-
-    # ok - ES
     'country_of_origin' : 'ES',
-
     'posting_group':'01',
-    
     'profit_Center':None,
-
     'rappel_group':'K0',
-
     'indicator':'X', 
-
     'valuation_class':'ZVN0',
-
     'indicator1':'X',
-
     'organizacion_de_compras':'FLES',
-
-    # ok - CIF
-    'vendor_code' : elm['cif'],
-
-    # ok - 1
+    'vendor_code' : elm.get('cif'),
     'purchase_price' : 1,
-
-    # EUR
     'currency' : 'EUR',
-
-    'family':None,
-
+    'family': None,
     'empty' : None,
-
-    # ES
     'specific_country_of_origin_inforecord':'ES',
   }
