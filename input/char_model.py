4ee5d28b510aff2796a816ba4b61ae04963d328c
4YabPPb8 <jorge.nova@astara.com>
2023-04-27 10:13:55 +0200

    chore(accenture): queries

---
+++ input/char_model.py
@@ -0,0 +1,24 @@
+import utils
+
+output_file = 'Charact_PA_Model'
+
+query = """
+select 
+    distinct TRIM(vc."model") as "model"
+from
+	vehicle_classifications vc
+order by
+	"model"
+;
+"""
+
+mock_query_response = dict(
+  model=''
+)
+
+def serialized_data(elm = mock_query_response):
+  return {
+    'Clave de idioma': 'ES',
+    'Gran Modelo': utils.commercial_model(elm),
+    'Denominacion valor de  caracteristica': elm.get('model'),
+  }
