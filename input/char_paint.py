245dd54cce4b38f6ee93cacc4916c37864b85d9a
4YabPPb8 <jorge.nova@astara.com>
2023-04-27 10:13:21 +0200

    chore(goto): queries

---
+++ input/char_paint.py
@@ -0,0 +1,26 @@
+import utils
+
+output_file = 'Charact_PA_Paint'
+
+query = """
+select 
+	distinct lower(trim(v.color)) as color
+from
+	vehicles v
+where
+  color is not null
+order by
+	color
+;  
+"""
+
+mock_query_response = dict(
+  color=''
+)
+
+def serialized_data(elm = mock_query_response):
+  return {
+    'Clave de idioma': 'ES',
+    'Pintura/External color': utils.color_code(elm),
+    'Denominacion valor de  caracteristica': utils.color_desc(elm),
+  }
