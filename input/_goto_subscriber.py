2da51d18c00a1671ee8b9f07e26a92176cfab622
4YabPPb8 <jorge.nova@astara.com>
2023-04-27 11:50:39 +0200

    chore(goto): updated script

--- input/_goto_subscriber.py
+++ input/_goto_subscriber.py
@@ -9,12 +9,7 @@ select
 	c.surnames,
 	c.email,
 	c.phone,
-	-- personal phone
-	-- birthday
 	c.billing_address,
-	-- gdpr brand
-	-- gdpr dealer
-	-- company name
 	c.cif 
 from subscriptions s 
 join clients c on s.client_id = c.id 
@@ -25,40 +20,32 @@ where
 """
 
 mock_query_response = dict( 
-  id='', name='', surnames='', email='', phone='', billing_address='', cif='',
+  id='',
+  name='',
+  surnames='',
+  email='',
+  phone='',
+  billing_address='',
+  cif='',
 )
 
 def serialized_data(elm = mock_query_response):
   return {
-    # Deben ser subscripciones activas ?
-
     'CustomerMoveId': elm.get('id'),
-    
-    # Dé dónde sacamos esto?
-    'SalesforceId': '🔴',
-    
+    'SalesforceId': None,
     'FirstName': elm.get('name'),
     'LastName': elm.get('surnames'),
     'PersonEmail': elm.get('email'),
-    
-    # Duplicamos ? 
-    'Phone': '🔴', 
-
+    'Phone': None, 
     'Mobilephone': elm.get('phone'),
     'BirthDate': None,
     'PersonMailingCountryCode': 'ES',
     'PersonMailingStreet': f"{utils.user_address(elm)} {utils.user_address_details(elm)}",
     'PersonMailingCity': utils.user_city(elm),
     'PersonMailingPostCode': utils.user_material_code(elm),
-
-    # Qué es esto?
-    'GDPR Brand': '🔴', 
-    'GDPR Dealer': '🔴', 
-
-    # Sirve el nombre?
-    'CompanyName': '🔴', 
-    
-    
+    'GDPR Brand': None, 
+    'GDPR Dealer': None, 
+    'CompanyName': None, 
     'documentType': 'CIF',
     'documentNumber': elm.get('cif'),
     'territory': 'ES',
