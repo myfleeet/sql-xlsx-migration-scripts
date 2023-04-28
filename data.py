f73dabf6bd38cca62291324c3b095e22997cc137
4YabPPb8 <jorge.nova@astara.com>
2023-04-28 13:34:07 +0200

    chore(data): list of wrong addresses

--- data.py
+++ data.py
@@ -53,3 +53,48 @@ store_codes = dict(
   cardive='b2c',
   fleeet='b2b',
 )
+
+normalize_astara_addresses = {
+  'Av. de Bruselas, 32, 28108 Alcobendas, Madrid': [
+    'brusleas 20',
+    'brusel',
+  ],
+  'Calle de los Aragoneses, 3, 28108 Alcobendas, Madrid, España': [
+    'arsgoneses 3 alcobendas',
+    'aragoses 3',
+    'aragonés 3 ',
+    'aradoneses',
+    'calle ragoneses 3 alcobendas',
+    'aragone',
+    'aragoné'
+    'astara',
+    'nave alcobendas',
+    'nave-alcobendas',
+    'nave, alcobendas',
+    'calle a4agoneses 3, madrid.',
+    'nave fleeet',
+    'nave.'
+  ],
+  'Avenida Carlos Sainz, 51, 28914 Leganés, Madrid, España': [
+    '51 53',
+    'carlos sa',
+    'nave leganés',
+    'campa de leganes',
+    'campa legan',
+    'está en legan',
+    'el coche está en leganes', 
+    'leganés avenida carlos 52 53',
+    'leganés, avenida  carlos  sainz 51',
+    'legenés',
+    'vehículo en leganes',
+    'leganes.',
+    'leganés, españa',
+    # 
+    'grupo más-avenida carlos  sainz,51-leganés',
+    'grupo más, avenida carlos 51, leganés'
+  ],
+  'Calle Ricardo Tormo, 102, Leganés, España': [],
+  'Plaza de Pau Vila, 1, 08039 Barcelona, España': [],
+  'Avinguda de Can Prat, no 15, 08100 Mollet del Vallès, Barcelona, España': []
+}
+
