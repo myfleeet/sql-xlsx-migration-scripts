265e9f06823ca3f98a8a60863fc8468181314e45
4YabPPb8 <jorge.nova@astara.com>
2023-05-08 17:16:40 +0200

    chore(script): updated script

---
+++ input/goto_user_loading_layout.py
@@ -0,0 +1,67 @@
+import utils
+
+output_file = 'User Loading Layout'
+
+query = """
+select
+	c.id,
+	c.email,
+	c.store,
+	c."name",
+	c.surnames,
+	c.cif, 
+	c.phone,
+	c.billing_address,
+	c.contact_person,
+	c2.password_digest
+from clients c
+left join credentials c2 on c.credentials_id = c2.id
+; 
+"""
+
+mock_query_response = dict(
+  id='',
+  email='',
+  store='',
+  name='',
+  surnames='',
+  cif='',
+  phone='',
+  billing_address='',
+  contact_person='',
+  password_digest='',
+)
+
+def serialized_data(elm = mock_query_response):
+  return {
+    'CustomerMoveId': elm.get('id'),
+    'Email': elm.get('email'),
+    'user type (b2b/b2c)': elm.get('store'),
+    'User status (active/not active/etc..)': 'active',
+    'email verified': 'True',
+    'first name': elm.get('name'),
+    'last name': elm.get('surnames'),
+    'phone number': elm.get('phone'),
+    'address': f"{utils.user_address(elm)} {utils.user_address_details(elm)}",
+    'city ': utils.user_city(elm),
+    'postcode': utils.user_material_code(elm),
+    'country': 'ES',
+    'state': utils.user_city(elm),
+    'confirm recevie email': 'True',
+    'company name': None,
+    'contact person first name': elm.get('contact_person'),
+    'contact person last name': None,
+    'CIF': elm.get('cif'),
+    'Phone': None,
+    'Mobilephone': elm.get('phone'),
+    'BirthDate': None,
+    'PersonMailingCountryCode': 'ES',
+    'PersonMailingStreet': f"{utils.user_address(elm)} {utils.user_address_details(elm)}",
+    'PersonMailingCity': utils.user_city(elm),
+    'PersonMailingPostCode': utils.user_material_code(elm),
+    'GDPR Brand': None,
+    'GDPR Dealer': None,
+    'documentType': 'CIF',
+    'territory': 'ES',
+    'Hashed Password': elm.get('password_digest'),
+  }
