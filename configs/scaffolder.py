b47bbaf60387babec3fa3199f5fff2c64c73ce61
4YabPPb8 <jorge.nova@astara.com>
2023-05-11 14:36:03 +0200

    chore(util): updated script

--- configs/scaffolder.py
+++ configs/scaffolder.py
@@ -1,7 +1,18 @@
 import configs.settings as settings
 import os
 
+folders = dict(
+  goto='goto_acc',
+  sap='sap'
+)
+
+def define_folder_project_name(item):
+  if 'goto' in item:
+    return folders.get('goto')
+  if 'sap' in item:
+    return folders.get('sap')
+
 def create_folder():
-  for folder in ['goto', 'accenture']:
+  for folder in list(folders.keys()):
     for env in list(settings.DB.keys()):
-      os.makedirs(f'out/{folder}/{env}', exist_ok=True)
+      os.makedirs(f'out/{folders.get(folder)}/{env}', exist_ok=True)
