16b4b3c1e0be630b32f96a2139ed9b7b47123dec
4YabPPb8 <jorge.nova@astara.com>
2023-06-14 15:40:11 +0200

    wip(script): returning values

--- input/goto_subscriptions_loading_layout.py
+++ input/goto_subscriptions_loading_layout.py
@@ -1,6 +1,6 @@
 import utils.utils as utils
 
-output_file = 'Subscription Loading Layout'
+output_file = "Subscription Loading Layout"
 
 query = """
 select 
@@ -27,12 +27,14 @@ select
 	-- SUB
 	pd.base_amount,
 	s.subscription_pack_period,
-  s.subscription_pack_price,
+	s.subscription_pack_price,
+	pd.tax_rate, -- % percentage
 	c2.code,
 	c2.amount_off,
 	c2.percent_off,
+	c2.duration, -- once (1 month) | foverer (all)
 	s.attached_documents,
-  s.comment
+ 	s.comment
 from subscriptions s 
 left join vehicle_assignments va on va.subscription_id = s.id 
 left join subscription_items si on si.subscription_id  = s.id
@@ -46,79 +48,151 @@ left join coupons c2 on pd.coupon_id = c2.id
 """
 
 mock_query_response = dict(
-  sub_id='',
-  client_id='',
-  status='',
-  make='',
-  model='',
-  version='',
-  color='',
-  vin='',
-  delivery_address='',
-  delivery_at='',
-  delivery_note='',
-  cancel_requested_at='',
-  pick_up_address='',
-  pick_up_at='',
-  insurance_by='',
-  insurance_cost='',
-  method_type='',
-  failed_scoring='',
-  base_amount='',
-  subscription_pack_period='',
-  subscription_pack_price='',
-  code='',
-  amount_off='',
-  percent_off='',
-  attached_documents='',
+    sub_id="",
+    client_id="",
+    status="",
+    make="",
+    model="",
+    version="",
+    color="",
+    vin="",
+    delivery_address="",
+    delivery_at="",
+    delivery_note="",
+    cancel_requested_at="",
+    pick_up_address="",
+    pick_up_at="",
+    insurance_by="",
+    insurance_cost="",
+    method_type="",
+    failed_scoring="",
+    base_amount="",
+    subscription_pack_period="",
+    subscription_pack_price="",
+    code="",
+    amount_off="",
+    percent_off="",
+    attached_documents="",
+    tax_rate="",
+    duration="",
 )
 
-def serialized_data(elm = mock_query_response):
-  return {
-    'SubscriptionMoveId': elm.get('sub_id'),
-    'SubscriptionSFDCId': None,
-    'CustomerMoveId': elm.get('client_id'),
-    'CustomerSFDCId': None,
-    'CompanyCode': None,
-    'Status': elm.get('status'),
-    'Substatus': None,
-    'SubscriptionOrigin': None,
-    'Brand': elm.get('make'),
-    'family': utils.family_code(elm),
-    'model': elm.get('model'),
-    'version': elm.get('version'),
-    'modelyear': None,
-    'color': elm.get('color'),
-    'upholstery': None,
-    'VIN': elm.get('vin'),
-    'deliveryType': utils.type_of_delivery(elm.get('delivery_address')),
-    'deliveryDate': utils.time_format(elm.get('delivery_at')),
-    'DeliveryHour': utils.hours_format(elm.get('delivery_at')),
-    'DeliveryPickupaddress': utils.normalize_address(elm.get('delivery_address')),
-    'DeliveryComments': elm.get('delivery_note'),
-    'RequestReturnDate': elm.get('cancel_requested_at'),
-    'PlannedReturnDate': elm.get('cancel_requested_at'),
-    'returnDate': utils.time_format(elm.get('pick_up_at')),
-    'returnHour': utils.hours_format(elm.get('pick_up_at')),
-    'returnAddress': utils.normalize_address(elm.get('pick_up_address')),
-    'ReturnType': utils.type_of_delivery(elm.get('pick_up_address')),
-    'pickupDate': elm.get('pick_up_at'),
-    'insurance': elm.get('insurance_by'),
-    'paymentMethod': elm.get('method_type'),
-    'EndingReason': None,
-    'scoring': None,
-    'ProbIncumplimientoScore': 'True' if elm.get('failed_scoring') else None,
-    'Nota': None,
-    'Decil': None,
-    'Percentil': None,
-    'deposit': None,
-    'period': elm.get('subscription_pack_period') or 1,
-    'extraInsuranceCost': elm.get('insurance_cost'), # EXTRA ? 
-    'subscriptionCost': elm.get('subscription_pack_price') if elm.get('subscription_pack_period') else elm.get('base_amount'),
-    'PromoCode': elm.get('code'),
-    'PromoDisccount': utils.promo_discount(elm),
-    'Comments': elm.get('comment'),
-    'History Json': None,
-    'Current Contracts': utils.json_to_str(elm.get('attached_documents')),
-  }
 
+def taxify(elm, value):
+    return f"{value} * {elm.get('tax_rate')}%"
+
+def get_cuota_actual(elm):
+    # if no discount
+    if not elm.get("code"):
+      return {
+          "1": f"{utils.get_price(elm)} + {elm.get('tax_rate')}",
+          "2": f"{utils.get_price(elm)} + {elm.get('tax_rate')}",
+      }
+        
+    # if discount
+    if elm.get("duration") == "once":
+        if elm.get("amount_off"):
+            return {
+                "1": f"{utils.get_price(elm)} + {elm.get('tax_rate')} + {elm.get('amount_off')} €",
+                "2": f"{utils.get_price(elm)} + {elm.get('tax_rate')}",
+            }
+        if elm.get("percent_off"):
+            return {
+                "1": f"{utils.get_price(elm)} + {elm.get('tax_rate')} + {elm.get('percent_off')} %",
+                "2": f"{utils.get_price(elm)} + {elm.get('tax_rate')}",
+            }
+    if elm.get("duration") == "forever":
+        if elm.get("amount_off"):
+            return {
+                "1": None,
+                "2": f"{utils.get_price(elm)} + {elm.get('tax_rate')} + {elm.get('amount_off')} €",
+            }
+        if elm.get("percent_off"):
+            return {
+                "1": None,
+                "2": f"{utils.get_price(elm)} + {elm.get('tax_rate')} + {elm.get('percent_off')} %",
+            }
+
+
+
+# ✅ OK
+def get_subtotal_plus_discount(elm):
+    # if not discount
+    if not elm.get("code"):
+      return utils.get_price(elm)
+    # if discount
+    if elm.get("amount_off"):
+        return utils.get_price(elm) - elm.get('amount_off')
+    if elm.get("percent_off"):
+        return utils.get_price(elm) - (utils.get_price(elm) * (elm.get('percent_off') / 100))
+    
+
+# ✅ OK
+def get_tax_rate(elm):
+    if(elm.get('tax_rate')):
+      return get_subtotal_plus_discount(elm) * elm.get('tax_rate')
+
+
+def serialized_data(elm=mock_query_response):
+    return {
+        # 'SubscriptionMoveId': elm.get('sub_id'),
+        # 'SubscriptionSFDCId': None,
+        # 'CustomerMoveId': elm.get('client_id'),
+        # 'CustomerSFDCId': None,
+        # 'CompanyCode': None,
+        # 'Status': elm.get('status'),
+        # 'Substatus': None,
+        # 'SubscriptionOrigin': None,
+        # 'Brand': elm.get('make'),
+        # 'family': utils.family_code(elm),
+        # 'model': elm.get('model'),
+        # 'version': elm.get('version'),
+        # 'modelyear': None,
+        # 'color': elm.get('color'),
+        # 'upholstery': None,
+        # 'VIN': elm.get('vin'),
+        # 'deliveryType': utils.type_of_delivery(elm.get('delivery_address')),
+        # 'deliveryDate': utils.time_format(elm.get('delivery_at')),
+        # 'DeliveryHour': utils.hours_format(elm.get('delivery_at')),
+        # 'DeliveryPickupaddress': utils.normalize_address(elm.get('delivery_address')),
+        # 'DeliveryComments': elm.get('delivery_note'),
+        # 'RequestReturnDate': elm.get('cancel_requested_at'),
+        # 'PlannedReturnDate': elm.get('cancel_requested_at'),
+        # 'returnDate': utils.time_format(elm.get('pick_up_at')),
+        # 'returnHour': utils.hours_format(elm.get('pick_up_at')),
+        # 'returnAddress': utils.normalize_address(elm.get('pick_up_address')),
+        # 'ReturnType': utils.type_of_delivery(elm.get('pick_up_address')),
+        # 'pickupDate': elm.get('pick_up_at'),
+        # 'insurance': elm.get('insurance_by'),
+        # 'paymentMethod': elm.get('method_type'),
+        # 'EndingReason': None,
+        # 'scoring': None,
+        # 'ProbIncumplimientoScore': 'True' if elm.get('failed_scoring') else None,
+        # 'Nota': None,
+        # 'Decil': None,
+        # 'Percentil': None,
+        # 'deposit': None,
+        # 'period': elm.get('subscription_pack_period') or 1,
+        # 'extraInsuranceCost': elm.get('insurance_cost'), # EXTRA ?
+
+        # Cuota actual                -> IF FOREVER -> subscriptionCost + IVA + descuento && IF ONCE -> subscriptionCost + IVA
+        # Cuota primer mes            -> IF FOREVER -> None && ONCE -> subscriptionCost + IVA + descuento
+        # Subtotal sin descuento      -> subscriptionCost - IVA
+        # Subtotal con descuento      -> subscriptionCost - IVA + descuento
+        # IVA                         -> ((subscriptionCost + descuento) * tax rate) / 100
+        "PromoCode": elm.get("code"),
+        "PromoDuration": elm.get("duration"),
+        "PromoDisccount": utils.promo_discount(elm),                 # -> ✅ 🔥 [MAIN]
+
+        "CuotaPrimerMes": get_cuota_actual(elm).get("1"),            # -> IF FOREVER -> None                                    && ONCE -> subscriptionCost + IVA + descuento
+        "CuotaActual": get_cuota_actual(elm).get("2"),               # -> IF FOREVER -> subscriptionCost + IVA + descuento      && IF ONCE -> subscriptionCost + IVA
+        "SubtotalSinDescuento": utils.get_price(elm),                # -> ✅ 🔥 [MAIN] subscriptionCost
+        "SubtotalConDescuento": get_subtotal_plus_discount(elm),     # -> ✅ subscriptionCost + descuento
+        "tax_rate": elm.get("tax_rate"),
+        "IVA": get_tax_rate(elm),                                    # -> ✅ ((subscriptionCost + descuento) * tax rate) / 100
+
+
+        # "Comments": elm.get("comment"),
+        # "History Json": None,
+        # "Current Contracts": utils.json_to_str(elm.get("attached_documents")),
+    }
